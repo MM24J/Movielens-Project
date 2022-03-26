6de9cd7e4ff1c05c171944fde5763d0e87f215bf
MM24J <68344724+MM24J@users.noreply.github.com>
2022-03-26 15:56:49 -0400

    Add files via upload

--- Movielens Project Code.R
+++ Movielens Project Code.R
@@ -3,10 +3,6 @@
 ##########################################################
 
 
-if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
-if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
-if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
-
 library(tidyverse)
 library(caret)
 library(data.table)
